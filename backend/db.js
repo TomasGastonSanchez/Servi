55002f46488b7d628d15099351208dc1fc941737
Tomás Sanchez <tomasgastonsanchez2002@gmail.com>
2025-03-24 22:59:46 -0300

    despegando la bd con railway

--- backend/db.js
+++ backend/db.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2');
+/*const mysql = require('mysql2');
 
 const conexion = mysql.createConnection({
     host: 'localhost',      
@@ -9,6 +9,27 @@ const conexion = mysql.createConnection({
 });
 
 //Manejo de errores
+conexion.connect((error) => {
+    if (error) {
+        console.error('Error al conectar a la base de datos:', error);
+        return;
+    }
+    console.log('Conexión a la base de datos establecida');
+});
+
+module.exports = conexion;
+*/
+
+const mysql = require('mysql2');
+
+const conexion = mysql.createConnection({
+    host: 'maglev.proxy.rlwy.net',      
+    user: 'root',       
+    password: 'udIJvtsosxyetdXWXCZuHNrGWZwmVxME', //la contraseña de railway
+    database: 'railway',
+    port: 46637,
+});
+
 conexion.connect((error) => {
     if (error) {
         console.error('Error al conectar a la base de datos:', error);
